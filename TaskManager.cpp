5274fd67d61a9872dac1669017ac53035ccdfd37
karm-4-uni <karm.hijazi@campus.technion.ac.il>
2025-06-21 21:39:35 +0300

    Add bumpPriorityByType function

--- TaskManager.cpp
+++ TaskManager.cpp
@@ -25,3 +25,27 @@ void TaskManager::completeTask(const string &personName) {
     }
         persons[index].completeTask();
 }
+
+void TaskManager::bumpPriorityByType(TaskType type, int priority) {
+    if(priority < 0 ) {
+        int index = -1 , i = 0 , j=0;
+        for (SortedList<Person>::ConstIterator it = persons.begin(); it != persons.end();++it , i++) {
+            SortedList<Task>::ConstIterator it2 = (*it).getTasks().begin();
+            SortedList<Task> new_tasks;
+while (it2 != (*it).getTasks().end() ) {
+    if((*it2).getType() == type) {
+        Task new_task((*it2).getPriority()+priority,
+            (*it2).getType(),(*it2).getDescription());
+        new_tasks.insert(new_task);
+        ++it2;
+    } else {
+        new_tasks.insert((*it2));
+        ++it2;
+    }
+}
+ persons[i].setTasks(new_tasks);
+        }
+    }   else {
+
+    }
+}
